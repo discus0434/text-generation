dec361787084e1c9610758b333a5c437d2a7876a
discus0434 <discus0434@gmail.com>
2022-07-17 13:41:44 +0000

    update

--- finetune.sh
+++ finetune.sh
@@ -2,10 +2,18 @@
 python gpt2-japanese/encode_bpe.py --src_dir sample_tweet --dst_file finetune
 
 # Fine-Tuning
-python gpt2-japanese/run_finetune.py --base_model gpt2ja-small --dataset finetune.npz --run_name gpt2ja-finetune-small
+python gpt2-japanese/run_finetune.py \
+    --base_model gpt2ja-small \
+    --dataset finetune.npz \
+    --run_name gpt2ja-finetune-small
 
 # Generate
-python gpt2-japanese/gpt2-generate.py --model gpt2ja-small/gpt2ja-finetune-small --num_generate 10
+python gpt2-japanese/gpt2-generate.py \
+    --model gpt2ja-small/gpt2ja-finetune-small \
+    --num_generate 10 \
+    --top_k 40 \
+    --top_p 0.9 \
+    --temperature 0.7
 
 # Remove pickle files
 for i in `seq 0 7`; do rm tmp$i.pkl; done
--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
+# Download GPT-2-ja small
 wget https://www.nama.ne.jp/models/gpt2ja-small.tar.bz2
 tar xvfj gpt2ja-small.tar.bz2
 
+# Optional (not needed)
 git clone https://github.com/huggingface/transformers
