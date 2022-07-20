ac863d29aa0e2ce577eec604ac24f22990b5e286
discus0434 <discus0434@gmail.com>
2022-07-20 03:14:07 +0000

    update

--- scripts/config/ng_words.py
+++ scripts/config/ng_words.py
@@ -1,12 +1,24 @@
+"""This is a custom NG-words list.
+If a generated sentence includes a word in a list, the model trys to re-generate.
+"""
+
 NG_WORDS = [
+
+    # Unscrupulous words
     "死",
     "亡",
     "原発",
     "原爆",
     "被災",
     "戦災",
+
+    # Discriminatory words
     "障害",
     "発達",
     "アスぺ",
     "インセル",
+    "左翼",
+    "右翼",
+    "ネトウヨ",
+    "パヨク",
 ]
