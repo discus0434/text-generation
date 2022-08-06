a3c927a7faf6f3442b7fe71f90e71b946457f1b6
discus0434 <discus0434@gmail.com>
2022-08-06 00:19:41 +0000

    update

--- scripts/shell/generate.sh
+++ scripts/shell/generate.sh
@@ -3,9 +3,9 @@
 # Generate
 mkdir dist -p
 python gpt2-japanese/gpt2-generate.py \
-    --model checkpoints/gpt2ja-finetune-small \
-    --num_generate 10 \
+    --model checkpoints/gpt2ja-2022-07-26-finetune-small \
+    --num_generate 1000 \
     --temperature 1.0 \
-    --min_length 150 \
-    --max_length 250 \
-    --output_file dist/dist.txt
+    --min_length 120 \
+    --max_length 139 \
+    --output_file dist/out.txt
